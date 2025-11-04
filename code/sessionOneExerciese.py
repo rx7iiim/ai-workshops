9fe2ebf4162339fb59a59ea8be381b3ad55615fa
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-11-04 23:15:14 +0100

    python session exercice

---
+++ code/sessionOneExerciese.py
@@ -0,0 +1,19 @@
+emails = [
+    "Win a free iPhone now! Click this link!",
+    "Meeting tomorrow at 10 AM.",
+    "Limited offer!!! Get 90%,discount!",
+    "Please find attached the project report.",
+    "You won a lottery! Claim your prize now!",
+    "Let's schedule our weekly sync-up.",
+    "Exclusive deal just for you!",
+    "Your flight ticket is confirmed.",
+    "Earn money fast with this trick!",
+    "See you at the office today."
+]
+
+
+def classify_email():
+      print("write your code here. remember to delete me first :)")
+   
+
+
