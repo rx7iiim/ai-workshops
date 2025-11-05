e62c65b84af7919e9cc4a4031f4e28e4029eb0fc
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-11-05 23:28:26 +0100

    Update sessionOneExerciese.py

--- code/sessionOneExerciese.py
+++ code/sessionOneExerciese.py
@@ -1,7 +1,7 @@
 emails = [
     "Win a free iPhone now! Click this link!",
     "Meeting tomorrow at 10 AM.",
-    "Limited offer!!! Get 90% discount!",
+    "Limited offer!!! Get 90%,discount!",
     "Please find attached the project report.",
     "You won a lottery! Claim your prize now!",
     "Let's schedule our weekly sync-up.",
@@ -11,12 +11,18 @@ emails = [
     "See you at the office today."
 ]
 
-spam_keywords = ['free', 'offer', 'iphone', 'link', 'lottery', '!!!']
+spam_keywords = ['free', 'offer', 'iphone', 'link', 'lottery', '!!!', 'earn', 'deal', 'discount']
 
 for email in emails:
-    text = email.lower()  # make it case-insensitive
-    # check if any spam word appears in the email text
-    if any(keyword in text for keyword in spam_keywords):
-        print(f'"{email}" → SPAM 🚫')
+    text = email.lower()
+    is_spam = False
+
+    for word in spam_keywords:
+        if word in text:
+            is_spam = True
+            break
+
+    if is_spam:
+        print(email, "SPAM")
     else:
-        print(f'"{email}" → NOT SPAM ✅')
+        print(email, " NOT SPAM")
